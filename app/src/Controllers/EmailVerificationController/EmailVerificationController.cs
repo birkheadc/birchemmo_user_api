b90019bffd30062e6a9660f1d374da6086ea0373
Colby Birkhead <birkheadc@gmail.com>
2023-02-16 13:35:09 +0900

    add SendEmail endpoint to EmailVerificationController

--- app/src/Controllers/EmailVerificationController/EmailVerificationController.cs
+++ app/src/Controllers/EmailVerificationController/EmailVerificationController.cs
@@ -1,3 +1,4 @@
+using BircheMmoUserApi.Filters;
 using BircheMmoUserApi.Models;
 using BircheMmoUserApi.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -10,31 +11,16 @@ namespace BircheMmoUserApi.Controllers;
 public class EmailVerificationController : ControllerBase
 {
   private readonly IEmailVerificationService emailVerificationService;
+  private readonly IEmailService emailService;
 
-  public EmailVerificationController(IEmailVerificationService emailVerificationService)
+  public EmailVerificationController(IEmailVerificationService emailVerificationService, IEmailService emailService)
   {
     this.emailVerificationService = emailVerificationService;
-  }
-
-  [HttpGet]
-  [Route("{verificationCode}")]
-  public async Task<IActionResult> VerifyEmailGet([FromRoute] string verificationCode)
-  {
-    try
-    {
-      TokenWrapper token = new(verificationCode);
-      bool isVerified = await emailVerificationService.ValidateUser(token);
-      if (isVerified == true) return Ok("Your code worked!");
-      return Ok("Your code didn't work :(");
-    }
-    catch
-    {
-      return Ok("Something went wrong!");
-    }
+    this.emailService = emailService;
   }
 
   [HttpPost]
-  [Route("{verificationCode}")]
+  [Route("verify/{verificationCode}")]
   public async Task<IActionResult> VerifyEmail([FromRoute] string verificationCode)
   {
     try
@@ -48,5 +34,17 @@ public class EmailVerificationController : ControllerBase
     {
       return StatusCode(9001);
     }
+  }
+
+  [HttpPost]
+  [Route("send")]
+  [SessionTokenAuthorize(Role.UNVALIDATED_USER)]
+  public async Task<IActionResult> SendEmail()
+  {
+    UserModel? user = HttpContext.Items["requestUser"] as UserModel;
+    if (user is null) return Unauthorized();
+    bool didSend = await emailService.SendVerificationEmail(user);
+    if (didSend == true) return Ok();
+    return BadRequest();
   }
 }
