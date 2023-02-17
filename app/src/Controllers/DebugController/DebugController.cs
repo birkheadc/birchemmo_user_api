153e80931952566990d22e09ca092f3e9232f679
Colby Birkhead <birkheadc@gmail.com>
2023-02-17 15:15:59 +0900

    remove old endpoints from debug controller

--- app/src/Controllers/DebugController/DebugController.cs
+++ app/src/Controllers/DebugController/DebugController.cs
@@ -24,44 +24,4 @@ public class DebugController : ControllerBase
   {
     return Ok("If you can see this, you've reached the debug controller.");
   }
-
-  [HttpPost]
-  [Route("test-email")]
-  public async Task<IActionResult> SendTestEmail()
-  {
-    try
-    {
-      bool didSend = await emailService.SendEmailAsync("Master Colby", "birkheadc@gmail.com", "Test Email", "This is just a test! If you're seeing this, it worked!");
-      return didSend ? Ok() : StatusCode(9002);
-    }
-    catch
-    {
-      return StatusCode(9001);
-    }
-  }
-
-  [HttpPost]
-  [Route("test")]
-  public async Task<IActionResult> Test()
-  {
-    try
-    {
-      NewUserModel newUser = new(
-        new Credentials(
-          "oldcheddar",
-          "password"
-        ),
-        "birkheadc@gmail.com"
-      );
-      UserModel? user = await userService.CreateUser(newUser);
-      if (user is null) return StatusCode(9003);
-
-      bool didSend = await emailService.SendVerificationEmail(user);
-      return didSend ? Ok() : StatusCode(9002);
-    }
-    catch(Exception e)
-    {
-      return BadRequest(e.Message);
-    }
-  }
 }
